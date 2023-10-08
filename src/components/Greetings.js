a1c81d4751d9acabd2067d67b08a47fb30bc3387
Radhika Senthil Kumar <radhee.senthil@gmail.com>
2023-10-08 15:05:05 -0500

    Added Greetings Name Props

--- src/components/Greetings.js
+++ src/components/Greetings.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import { View, Text } from 'react-native';
 
-const Greetings = () => {
+const Greetings = ({name = ""}) => {
   return (
     <View>
-      <Text>Hello!</Text>
+      <Text>Hello {name}!</Text>
     </View>
   );
 };
